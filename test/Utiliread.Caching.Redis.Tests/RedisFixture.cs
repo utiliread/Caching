2d48f008a9d25bc01cfc9fe21b74de53000defe8
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2022-01-10 10:21:58 +0100

    Let redis cleanup in fixture be scoped to fixture instance number

--- test/Utiliread.Caching.Redis.Tests/RedisFixture.cs
+++ test/Utiliread.Caching.Redis.Tests/RedisFixture.cs
@@ -2,24 +2,24 @@ using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
 using StackExchange.Redis;
 using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
-using RedisCacheOptions = Microsoft.Extensions.Caching.StackExchangeRedis.RedisCacheOptions;
 
 namespace Utiliread.Caching.Redis.Tests.Infrastrcuture
 {
     public class RedisFixture : IAsyncLifetime
     {
+        private static int _fixtureCount = 0;
+        private readonly int _fixtureNumber;
+        private int _cacheCount = 0;
         private ConnectionMultiplexer _connection;
-        private static int _instanceNumber = 0;
-        private static readonly ConcurrentDictionary<IDistributedCache, int> _index = new();
-        private static readonly ConcurrentDictionary<IDistributedCache, IServiceProvider> _services = new();
+        private readonly Dictionary<IDistributedCache, (int CacheNumber, IServiceProvider Services)> _caches = new();
 
         private const string CleanupScript = @"
-local keys = redis.call('KEYS', 'TagableCacheTestFixture:*')
+local keys = redis.call('KEYS', 'TagableCacheTestFixture:fixture-{{fixtureNumber}}:*')
 if #keys > 0 then
     local i = 1
     while i <= #keys-1000 do
@@ -32,64 +32,68 @@ return 0";
 
         public IDatabase Database { get; private set; }
 
+        public RedisFixture()
+        {
+            _fixtureNumber = Interlocked.Increment(ref _fixtureCount);
+        }
+
         public async Task InitializeAsync()
         {
             _connection = await ConnectionMultiplexer.ConnectAsync("localhost");
             Database = _connection.GetDatabase();
 
-            await Database.ScriptEvaluateAsync(CleanupScript);
+            await RunCleanupAsync();
         }
 
         public IDistributedCache CreateCacheInstance()
         {
-            var instanceNumber = Interlocked.Increment(ref _instanceNumber);
-
+            var instanceNumber = ++_cacheCount;
             var services = new ServiceCollection()
                 .AddUtilireadRedisCache(options =>
                 {
                     options.Configuration = "localhost";
-                    options.InstanceName = $"TagableCacheTestFixture:{instanceNumber}:";
+                    options.InstanceName = $"TagableCacheTestFixture:fixture-{_fixtureNumber}:{instanceNumber}:";
                 })
                 .BuildServiceProvider();
 
             var instance = services.GetRequiredService<IDistributedCache>();
-
-            _index[instance] = instanceNumber;
-            _services[instance] = services;
-
+            _caches[instance] = (instanceNumber, services);
             return instance;
         }
 
         public string GetInstanceName(IDistributedCache cache)
         {
-            var instanceNumber = _index[cache];
-            return $"TagableCacheTestFixture:{instanceNumber}:";
+            var instanceNumber = _caches[cache].CacheNumber;
+            return $"TagableCacheTestFixture:fixture-{_fixtureNumber}:{instanceNumber}:";
         }
 
         public Task<string[]> GetKeysAsync(IDistributedCache cache)
         {
-            var instanceNumber = _index[cache];
-
+            var instanceNumber = _caches[cache].CacheNumber;
             var server = _connection.GetServer(_connection.GetEndPoints().First());
 
-            var keys = server.Keys(pattern: $"TagableCacheTestFixture:{instanceNumber}:*");
+            var keys = server.Keys(pattern: $"TagableCacheTestFixture:fixture-{_fixtureNumber}:{instanceNumber}:*");
 
             return Task.FromResult(keys.ToArray().Select(x => (string)x).ToArray());
         }
 
         public async Task RunExpireAsync(IDistributedCache cache)
         {
-            var expirer = _services[cache].GetRequiredService<RedisExpirer>();
-
+            var expirer = _caches[cache].Services.GetRequiredService<RedisExpirer>();
             await expirer.EnsureConnectionAsync();
             await expirer.RunExpireAsync();
         }
 
         public async Task DisposeAsync()
         {
-            await Database.ScriptEvaluateAsync(CleanupScript);
+            await RunCleanupAsync();
 
             _connection.Dispose();
+        }
+
+        private Task RunCleanupAsync()
+        {
+            return Database.ScriptEvaluateAsync(CleanupScript.Replace("{{fixtureNumber}}", _fixtureNumber.ToString()));
         }
     }
 }
